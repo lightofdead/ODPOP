6f0b6810f21d36af1c5a2a608c57dfa782c70570
lightofdead <75844154+lightofdead@users.noreply.github.com>
2023-03-09 18:16:26 +0700

    Классы Programm
    Класс Programm разбит на отдельные классы

---
+++ Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1.Classes
+{
+    internal class Film : MediaBasic
+    {
+        protected string producer;
+        public Film(string title, string produser) : base(title)
+        {
+            this.producer = produser;
+        }
+    }
+}
